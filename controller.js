8c29b2a87d75baee80f5490b2ae9b77e42b123ba
Balaji Rahunathan <32451142+Balaji-Rahunathan@users.noreply.github.com>
2023-02-24 08:33:07 +0530

    Read from json file

--- controller.js
+++ controller.js
@@ -1,24 +1,34 @@
-// data
-const qrData = [
-  'agr-1-111',
-  'agr-2-111',
-];
+//get Data from file
+function getDataArray() {
+  let dataArray = []
+  fetch('./Keeladi.json')
+    .then((response) => response.json())
+    .then((json) => {
+      json.map(data => {
+        if (data["Artefact Name / Number (Scanned File Number)"] !== "") {
+          dataArray.push(data["Artefact Name / Number (Scanned File Number)"].toLowerCase())
+        }
+      })
+      // console.log(dataArray)
+      generateQR(dataArray)
+    });
+}
 
-async function generateQR() {
+async function generateQR(qrData) {
   // Loop through data and generate QR code for each item
   await qrData.map((data, index) => {
     let qrContainer = document.createElement("div")
     qrContainer.className = "qr-code-container"
-    qrContainer.id = "container"+index
+    qrContainer.id = "container" + index
 
     let qrImageContainer = document.createElement("div")
     qrImageContainer.className = "qrcode-image-container"
-    qrImageContainer.id = "qrimage"+index
+    qrImageContainer.id = "qrimage" + index
 
     qrContainer.appendChild(qrImageContainer)
     document.body.appendChild(qrContainer)
 
-    new QRCode(document.getElementById("qrimage"+index), {
+    new QRCode(document.getElementById("qrimage" + index), {
       text: data,
       width: 610,
       height: 610,
@@ -28,21 +38,22 @@ async function generateQR() {
     });
 
     const qrCodeContainer = document.getElementById("container" + index);
-    printDiv2(qrCodeContainer,data)
+    printToDiv(qrCodeContainer, data)
   });
 }
 
-function printDiv2(div,name) {
+function printToDiv(div, name) {
   html2canvas(div).then(function (canvas) {
     var img = canvas.toDataURL();
-    downloadURI("data:" + img, name+".png");
+    startDownload("data:" + img, name + ".png");
   });
 }
-function downloadURI(uri, name) {
+function startDownload(uri, name) {
   var link = document.createElement("a");
   link.download = name;
   link.href = uri;
   link.click();
 }
 
-generateQR()
+
+getDataArray()
